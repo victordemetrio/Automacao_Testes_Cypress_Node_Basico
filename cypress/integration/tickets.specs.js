e8ea9035535574a07889d4df220a9af2150c4757
Victor Demetrio <victordemetrio@gmail.com>
2020-09-19 21:06:18 -0300

    Cypress rum + test has_'TICKETBOX'_header's_heading

---
+++ cypress/integration/tickets.specs.js
@@ -0,0 +1,6 @@
+describe("Tickets",() => {
+    beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
+
+    it("has 'TICKETBOX' header's heading", () => {});
+
+});
